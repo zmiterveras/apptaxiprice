26b5eef69ca4a17433f364b9ccc2690b391c658c
ZMV <zmvph79@gmail.com>
2020-11-15 17:45:53 +0300

    to end: response.html, app.route(count); add func-counter

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template
 from flask_wtf import FlaskForm
 from wtforms import IntegerField, FloatField, SubmitField
-
+from wtforms.validators import DataRequired
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'hard to guess string'
@@ -14,9 +14,16 @@ class NameForm(FlaskForm):
     med_trip_state = IntegerField('Средняя поездка трасса, км', validators=[DataRequired()])
     taxi_rate_city = FloatField('Тариф город, руб', validators=[DataRequired()])
     taxi_rate_state = FloatField('Тариф трасса, руб', validators=[DataRequired()])
-    rate_boarding_ = FloatField('Стоимость посадки, руб', validators=[DataRequired()])
+    rate_boarding = FloatField('Стоимость посадки, руб', validators=[DataRequired()])
     include_trip = IntegerField('Включенные км', validators=[DataRequired()])
     submit = SubmitField('Рассчитать')
+
+
+def counter(name):
+    p_city = (name[0] / name[2]) * name[6] + (name[0] - name[7] * (name[0] / name[2])) * name[4]
+    p_route = (name[1] / name[3]) * name[6] + (name[1] - name[7] * (name[1] / name[3])) * name[5]
+    p = p_city + p_route
+    return p_city, p_route, p
 
 
 @app.route('/')
@@ -29,13 +36,29 @@ def about():
     return render_template('about.html')
 
 
-@app.route('/count')
+@app.route('/count', methods=['GET', 'POST'])
 def count():
-    return render_template('count.html')
+    name = []
+    form = NameForm()
+    if form.validate_on_submit():
+        pr_city = form.pr_city.data
+        pr_state = form.pr_state.data
+        med_trip_city = form.med_trip_city.data
+        med_trip_state = form.med_trip_state.data
+        taxi_rate_city = form.taxi_rate_city.data
+        taxi_rate_state = form.taxi_rate_state.data
+        rate_boarding = form.rate_boarding.data
+        include_trip = form.include_trip.data
+        name.extend([pr_city, pr_state, med_trip_city, med_trip_state, taxi_rate_city, taxi_rate_state, rate_boarding,
+                     include_trip])
+        p_city, p_state, p = counter(name)
+        return render_template('response.html', p_city=p_city, p_state=p_state, p=p)
+    else:
+        return render_template('count.html', form=form)
 
 
 @app.errorhandler(404)
-def page_not_found(e):
+def page_not_found():
     return render_template('page_not_found.html'), 404
 
 
